366c4ad06266ced497a624913be740fcb2948eec
joshuathomas2907 <joshua.thomas2907@gmail.com>
2020-04-22 17:11:13 +1000

    added achievement, level selector and information layouts

---
+++ app/src/main/java/com/example/gridgambit/AchievementsScreen.java
@@ -0,0 +1,13 @@
+package com.example.gridgambit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AchievementsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/gridgambit/InfoScreen.java
@@ -0,0 +1,14 @@
+package com.example.gridgambit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info_screen);
+    }
+}
